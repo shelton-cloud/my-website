e9d7446734e63236c29b60f1e9e752b6c233e72e
Bryan Shelton <bryan.shelton@gmail.com>
2026-02-07 22:55:25 -0800

    feat: Add Vercel Analytics to the application layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body className={inter.className}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
